06d32c1394470bd7899da08eb8ab562691d517da
Emmanuel Emmanuel <imma2005nuel@gmail.com>
2023-06-01 14:12:18 +0100

    add scripts.js variable functionalities

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const updateMenuScale = (scale) => {
+    menuWrap.style.transform = `scale(${scale})`;
+};
+
+const openMenu = () => updateMenuScale(1);
+const closeMenu = () => updateMenuScale(0);
+
+closeMenuBtn.addEventListener('click', closeMenu);
+startButton.addEventListener('click', openMenu);
+
+// Soccer Trivia
+const selectSoccerTrivia = () => {
+    closeMenu();
+    soccerTrivia.startTrivia();
+};
+soccerButton.addEventListener('click', () => selectSoccerTrivia());
+
+// Donald Trivia
+const selectDonaldTrivia = () => {
+    closeMenu();
+    donaldTrivia.startTrivia();
+};
+donaldButton.addEventListener('click', () => selectDonaldTrivia());
+
+// Anime Trivia
+const selectAnimeTrivia = () => {
+    closeMenu();
+    animeTrivia.startTrivia();
+};
+animeButton.addEventListener('click', () => selectAnimeTrivia());
